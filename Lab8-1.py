a23a8585faf6488a441743b03afbe55770d174e3
Natthapong Lueangphumphitthaya <thenat2015@gmail.com>
2022-02-24 18:43:32 +0700

    add 2

--- Lab8-1.py
+++ Lab8-1.py
@@ -23,7 +23,28 @@ class Simple_drawing_window1(QWidget):
         p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
 
+class Simple_drawing_window2(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle('Simple Drawing')
+        self.rabbit = QPixmap('rabbit.png')
 
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPolygon([
+            QPoint( 70, 100), QPoint(100, 110),
+            QPoint(130, 100), QPoint(100, 150),
+        ])
+
+        p.setPen(QColor(255, 0, 0 ))
+        p.setBrush(QColor(255, 0, 0 ))
+        p.drawPie(50, 150, 100, 100, 0, 360 * 16)
+        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
+        p.end()
 
 class Simple_drawing_window3(QWidget):
     def __init__(self):
